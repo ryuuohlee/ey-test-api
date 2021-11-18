48519fc6e12fb3d6deaf2fb00154f379bbe76f4c
Jeffrey Lee <jglee1@live.com>
2021-11-17 23:20:21 -0800

    Feature: starting api environment

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv')
+const express = require('express');
+
+
+//route configs
+const app = express();
+const port = 3000;
+
+//controllers
+
+
+//routes
+
+app.listen(port, () => {
+  console.log(`Port is listening on port ${port}`);
+})
